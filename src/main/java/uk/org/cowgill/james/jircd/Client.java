e90cdb0df69db93922b08a06ce5a5ba0239e3ffe
James Cowgill <james410@cowgill.org.uk>
2011-02-18 23:59:30 +0000

    Added eclipse project files and the client skeleton.

---
+++ src/main/java/uk/org/cowgill/james/jircd/Client.java
@@ -0,0 +1,85 @@
+package uk.org.cowgill.james.jircd;
+
+/**
+ * Represents a client in the server
+ * 
+ * Clients are users on the server with a nickname, modes and can join channels
+ * 
+ * This could be a remote client or a local servlet
+ * 
+ * @author James
+ */
+public abstract class Client
+{	
+	/**
+	 * Sends a string of IRC data to a client
+	 * 
+	 * @param data Data to send
+	 */
+	public abstract void send(String data);
+	
+	/**
+	 * Performs client sepific close routines
+	 * 
+	 * @return Returns true if the close was a sucess. Returns false to abort the close.
+	 */
+	protected abstract boolean rawClose();
+
+	/**
+	 * Changes the class of this client
+	 * @param clazz Class to change to
+	 * @param defaultClass True to change default class
+	 */
+	protected void changeClass(ConnectionClass clazz, boolean defaultClass)
+	{
+		//Default = no class changes
+	}
+	
+	/**
+	 * Restores this client's class to the default class
+	 */
+	public void restoreClass()
+	{
+		//Default = no class changes
+	}
+	
+	/**
+	 * Changes the class of this client
+	 * @param clazz Class to change to
+	 */
+	public final void changeClass(ConnectionClass clazz)
+	{
+		changeClass(clazz, false);
+	}
+	
+	/**
+	 * Returns the ip address for this client
+	 * 
+	 * Servlets always return 127.0.0.1
+	 * 
+	 * @return The ip address of the client
+	 */
+	public String ipAddress()
+	{
+		//Default return
+		return "127.0.0.1";
+	}
+	
+	/**
+	 * Returns true if this client is a remote user
+	 * 
+	 * @return true if this client is a remote user
+	 */
+	public final boolean isRemote()
+	{
+		return this instanceof NetworkClient;
+	}
+	
+	/**
+	 * Requests that this client be closed
+	 */
+	public void close()
+	{
+		//TODO Close
+	}
+}
